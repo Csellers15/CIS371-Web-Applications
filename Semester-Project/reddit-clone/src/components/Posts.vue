da2a0c113b5271bf4e25a87e3c7a618a8ed010d6
Cole Sellers <csellers18@yahoo.com>
2019-06-12 20:40:04 -0400

    added toggle for private data

--- Semester-Project/reddit-clone/src/components/Posts.vue
+++ Semester-Project/reddit-clone/src/components/Posts.vue
@@ -1,5 +1,6 @@
 <template>
 <div class="container">
+  <button type="submit" id="filterBtn" v-if="isAuth" @click="filter" value="Show Only My Posts"> Show only my posts  </button>
   <div class="posts card mt-5" v-for="p in postData" v-bind:key="p.id">
     <h2 class="card-header"> {{p.title}}</h2>
 
@@ -33,13 +34,35 @@ export default {
 
   methods: {
     dataHandler (snapshot) {
+      
       this.postData.push({fbKey: snapshot.key, ...snapshot.val()});
       this.postData.reverse();
+    },
+
+    filter: function(){
+      var elem = document.getElementById("filterBtn");
+      let temp = [];
+
+      if(elem.innerHTML != "Show all Posts"){
+        for(let i = 0; i < this.postData.length; i++){
+          if(this.postData[i].uId === firebase.auth().currentUser.uid){
+            temp.push(this.postData[i]);
+          } 
+        }
+        this.postData = temp;
+        elem.innerHTML = "Show all Posts"
+        
+      } else {
+        elem.innerHTML = "Show Only My Posts"
+        this.postData = [];
+        firebase.database().ref("posts").on("child_added", this.dataHandler);
+      }
     }
   },
   mounted() {
     firebase.database().ref("posts").on("child_added", this.dataHandler);
   },
+
 
 }
 </script>
